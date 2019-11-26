c3bb609d82d1b60c9f6492eba65680c4287130d5
dionybusker <99057959@mydavinci.nl>
2019-11-26 10:14:10 +0100

    pogingen elements toe te voegen

--- adventure.js
+++ adventure.js
@@ -1,3 +1,4 @@
+var gameContainer = document.getElementById("game-container");
 var title = document.getElementById("title");
 var story = document.getElementById("description");
 var button1 = document.getElementById("button1");
@@ -5,9 +6,19 @@ var button2 = document.getElementById("button2");
 var button3 = document.getElementById("button3");
 var inventory = document.getElementById("inventoryItem");
 
-title.innerHTML = "Mooi geel";
-story.innerHTML = "Mooi rood";
-button1.innerHTML = "Leuke button";
-button2.innerHTML = "Leuke button";
-button3.innerHTML = "Leuke button";
-inventory.src = "img/voorbeeld.jpg";
+title.innerText = "Mooi geel";
+story.innerText = "Mooi rood";
+button1.innerText = "Eerste button";
+button2.innerText = "Tweede button";
+button3.innerText = "Derde button";
+// inventory.src = "img/voorbeeld.jpg";
+
+
+var levelImage = document.createElement("img");
+levelImage.style.height = "auto";
+levelImage.style.width = "200px";
+levelImage.src = "img/voorbeeld.jpg";
+
+// gameContainer.appendChild(levelImage);
+
+// levelImage.innerHTML = "Mooi geel";
