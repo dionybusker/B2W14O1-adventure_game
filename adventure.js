b66201ab9c9a94ba4248386eac7f4730de555e5b
dionybusker <99057959@mydavinci.nl>
2019-11-26 09:28:14 +0100

    teksten aangepast als test

---
+++ adventure.js
@@ -0,0 +1,13 @@
+var title = document.getElementById("title");
+var story = document.getElementById("description");
+var button1 = document.getElementById("button1");
+var button2 = document.getElementById("button2");
+var button3 = document.getElementById("button3");
+var inventory = document.getElementById("inventoryItem");
+
+title.innerHTML = "Mooi geel";
+story.innerHTML = "Mooi rood";
+button1.innerHTML = "Leuke button";
+button2.innerHTML = "Leuke button";
+button3.innerHTML = "Leuke button";
+inventory.src = "img/voorbeeld.jpg";
