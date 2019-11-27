e7d8ad4f2cd13514a1d34f369a70e733bd7fb89c
dionybusker <99057959@mydavinci.nl>
2019-11-27 09:15:01 +0100

    level_1 toegevoegd

--- story.js
+++ story.js
@@ -1,3 +1,13 @@
 var levelTitle = document.getElementById("level");
 
-levelTitle.innerText = "Level 1";
+levelTitle.innerText = "Start";
+
+
+story.innerText = `Vandaag is het de dag, de dag waar jij je zo lang op hebt verheugd. Samen met je klas ga je op excursie naar een zeer bekend terrein dat echter niemand dan behalve de militaire basis ooit heeft mogen betreden. Gelukkig hebben jullie een docent die dit heeft kunnen regelen. \n \n Het terrein dat jullie gaan bezoeken ligt ver in het noorden van het land. Het is een groot, ongebruikt gebied. Zeggen ze… De militaire basis ligt ver verstopt tussen de bossen die er om heen staan. Een goede plek dus om geheimzinnige projecten uit te voeren. En misschien dat jullie de kans krijgen om één van deze projecten te mogen bekijken… \n \n De excursie naar Area 51 ½ is verplicht voor iedere student uit de klas A1. De bussen staan al klaar op de parkeerplaats en je staat te springen om eindelijk te kunnen vertrekken. Dus waar wacht je nog op?`;
+
+button1.innerText = `LET'S GO!`;
+button2.style.visibility = "hidden"
+button3.style.visibility = "hidden"
+
+
+var level_1 = `Je zit met je klasgenoten in de bus onderweg naar Area 51 ½. Je raakt maar niet uitgepraat met een andere student over mogelijke projecten die daar allemaal gedaan worden. De docent draait zich om en schraapt zijn keel. De hele klas is direct muisstil. \n \n “Wat fijn, zo’n rustige klas,” begint de docent. Hij begint te lachen en trekt dan weer een serieus gezicht. “Ik wilde nog even aan jullie mededelen dat het van belang is dat jullie je mobiele telefoons uitschakelen en opbergen in jullie tassen. Deze mogen tijdens de excursie absoluut niet gebruikt worden.” \n \n Je twijfelt om je mobiel uit te zetten. Wat nou als er iets gebeurd en je kan niemand bereiken? Of niemand kan jou bereiken? Daarnaast is het wel logisch dat het verboden is een mobiel te gebruiken op zo’n geheim terrein…`;
